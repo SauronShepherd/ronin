d58072db948df21704a4ea9288c344aed91845ce
__angel__ <angel.alvarez.pascua@gmail.com>
2026-09-07 08:52:59 +0200

    style: satisfy import ordering

--- python/studio_server/__init__.py
+++ python/studio_server/__init__.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 from studio_execution import DurableExecutionService, WorkerPollResult
-from studio_server.http import DurableHTTPApplication, RoninHTTPServer, SUPPORTED_ROUTES
+from studio_server.http import SUPPORTED_ROUTES, DurableHTTPApplication, RoninHTTPServer
 
 __all__ = (
     "DurableExecutionService",
