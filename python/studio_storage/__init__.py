7c95e8019996925abe5a4f3e5ced083b44365936
__angel__ <angel.alvarez.pascua@gmail.com>
2026-09-07 11:19:40 +0200

    feat: expose C0 paged storage adapters

--- python/studio_storage/__init__.py
+++ python/studio_storage/__init__.py
@@ -4,9 +4,9 @@ from __future__ import annotations
 
 from studio_storage.artifacts import ArtifactIntegrityError, ArtifactRef, LocalArtifactStore
 from studio_storage.async_artifacts import BoundedAsyncArtifactStore
-from studio_storage.async_store import BoundedAsyncJobStore, StorageBackpressureError
-from studio_storage.fenced_sqlite import SqliteJobStore
-from studio_storage.memory import IdempotencyConflict, InMemoryJobStore
+from studio_storage.async_store import StorageBackpressureError
+from studio_storage.memory import IdempotencyConflict
+from studio_storage.paged_store import BoundedAsyncJobStore, InMemoryJobStore, SqliteJobStore
 from studio_storage.sqlite import migrate, open_database, schema_version
 
 __all__ = (
