4f73e61d825f824f639c0fadc8a2727fa335a8de
__angel__ <angel.alvarez.pascua@gmail.com>
2026-09-07 08:50:38 +0200

    feat: expose HTTP adapter surface

--- python/studio_server/__init__.py
+++ python/studio_server/__init__.py
@@ -3,5 +3,12 @@
 from __future__ import annotations
 
 from studio_execution import DurableExecutionService, WorkerPollResult
+from studio_server.http import DurableHTTPApplication, RoninHTTPServer, SUPPORTED_ROUTES
 
-__all__ = ("DurableExecutionService", "WorkerPollResult")
+__all__ = (
+    "DurableExecutionService",
+    "DurableHTTPApplication",
+    "RoninHTTPServer",
+    "SUPPORTED_ROUTES",
+    "WorkerPollResult",
+)
