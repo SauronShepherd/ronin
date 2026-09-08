554cab4aed6b7b6ddc2dc6e330c0aa64dcbb2c77
__angel__ <angel.alvarez.pascua@gmail.com>
2026-09-08 06:35:17 +0200

    style: remove unused network import

--- python/studio_cli/network.py
+++ python/studio_cli/network.py
@@ -4,7 +4,6 @@ from __future__ import annotations
 
 import json
 from dataclasses import dataclass
-from typing import Any
 from urllib.error import HTTPError, URLError
 from urllib.parse import quote, urlencode, urlsplit
 from urllib.request import Request, urlopen
