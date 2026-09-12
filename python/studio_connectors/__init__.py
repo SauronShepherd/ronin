16ccc3ed5a5a90183380a19da279ad6b24ca6f13
__angel__ <angel.alvarez.pascua@gmail.com>
2026-09-12 21:54:52 +0200

    refactor: keep local connector inside existing storage package

--- python/studio_connectors/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Connector adapters for Ronin Public v1."""
-
-from .local_files import LocalFileConnector, LocalFileIngestionResult
-
-__all__ = ("LocalFileConnector", "LocalFileIngestionResult")
